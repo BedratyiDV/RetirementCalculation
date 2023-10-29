a2981c14db74d896a41dcd97dbf3449b9c73c807
BedratyiDV <bedrik2023@gmail.com>
2023-10-29 21:10:10 +0100

    Initial commit
    Initial commit 29/10/2023

---
+++ RetirementCalculation/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetirementCalculation
+{
+    /// <summary>
+    /// The delegate defining a method that has two input integer parameters and returns a double value
+    /// </summary>
+    
+    public delegate double PensionCalculation(int a, int b);
+
+    /// <summary>
+    /// The definition of Person class
+    /// </summary>
+   
+    public class Person
+    {
+        private string _name;
+        private int _age;
+        private int _seniority;
+        
+        /// <summary>
+        /// Public Get method for private name property
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+        }
+        /// <summary>
+        /// Public Get method for private age property
+        /// </summary>
+        public int Age
+        {
+            get => _age;
+        }
+        /// <summary>
+        /// Public Get method for private seniority property
+        /// </summary>
+        public int Seniority
+        {
+            get => _seniority;
+        }
+        /// <summary>
+        /// Constructor for creating a new Person object
+        /// </summary>
+        /// <param name="name"> </param> 
+        /// <param name="age"> </param> 
+        /// <param name="seniority"> </param> 
+        public Person(string name, int age, int seniority)
+        {
+            _name = name;
+            _age = age;
+            _seniority = seniority;
+           
+        }
+        /// <summary>
+        /// Method accepts delegate as an input parameter for pension calculation 
+        /// Also accepts Person's age and seniority for calculation
+        /// </summary>
+        /// <param name="ag"> </param> 
+        /// <param name="srt"> </param> 
+        /// <param name="calculation"> </param> 
+
+        public double GetRetiremetnValue(int ag, int srt, PensionCalculation calculation)
+        {
+            return (calculation(ag,srt));
+        }
+    }
+}
---
+++ RetirementCalculation/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+
+namespace RetirementCalculation
+{
+    internal class Program
+    {
+                static void Main(string[] args)
+        {
+          /// <summary>
+         /// Creation a new object of Person type
+         /// </summary>
+
+            Person person = new Person("DaMarcus Beasley", 65, 40);
+
+            /// <summary>
+            /// Calling different pension calculation methods for different countries 
+            /// </summary>
+
+           double result = person.GetRetiremetnValue(person.Age,person.Seniority, PensionCalculationUkraine);
+
+           Console.WriteLine($"The pension sum for {person.Name} equals {result:F2}");
+
+            result = person.GetRetiremetnValue(person.Age, person.Seniority, PensionCalculationCanada);
+
+            Console.WriteLine($"The pension sum for {person.Name} equals {result:F2}");
+
+            result = person.GetRetiremetnValue(person.Age, person.Seniority, PensionCalculationGermany);
+
+            Console.WriteLine($"The pension sum for {person.Name} equals {result:F2}");
+
+            /// <summary>
+            /// Defining method for calculating pension in Ukraine 
+            /// </summary>
+            /// <param name="age"> </param> 
+            /// <param name="seniority"> </param> 
+
+            static double PensionCalculationUkraine (int age, int seniority)
+            
+            {
+                return 7500 + 15000.01 * seniority/age; 
+            }
+            /// <summary>
+            /// Defining method for calculating pension in Canada 
+            /// </summary>
+            /// <param name="age"> </param> 
+            /// <param name="seniority"> </param> 
+
+            static double PensionCalculationCanada(int age, int seniority)
+
+            {
+                return 10000 + 20000.01 * seniority/age;
+            }
+            /// <summary>
+            /// Defining method for calculating pension in Germany 
+            /// </summary>
+            /// <param name="age"> </param> 
+            /// <param name="seniority"> </param> 
+
+            static double PensionCalculationGermany(int age, int seniority)
+
+            {
+                return 7500 + 15000 * (age - seniority)/1.33;
+            }
+
+        }
+    }
+}
+
+
+
